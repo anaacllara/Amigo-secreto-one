6dbda2c50549d378113cbffc76ceb3902c3eedb2
anaacllara <anaclarafc09@gmail.com>
2025-02-06 10:55:54 -0300

    agregar = adicionar amigos; limpar campo; atualizar amigos e sortear

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,19 +1,44 @@
 //array para armazenar os nomes 
-let lista=[];
+let amigos=[];
 let nomes;
-function agregar_amigos(){
-    nomes = document.getElementById('amigo');
-    if(lista.includes(nome)){
+function adicionarAmigo(){
+    nomes = document.getElementById('amigo').value;
+    if(amigos.includes(nomes)){
         alert('O nome já existe, por favor digitar outro');
     }else if(nomes.value = ''){
         alert('Por favor, insira um nome');
     }else{
-        lista.push(nomes);
+        amigos.push(nomes);
+        console.log(amigos);
+        limparCampo();
+        atualizarAmigos();
     }
-    limparCampo();
 }
 
 function limparCampo(){
     campo = document.querySelector('input');
     campo.value = '';
+}
+
+// atualizar lista de amigos 
+function atualizarAmigos(){
+    let lista = document.getElementById('listaAmigos');
+    for(let i = 1; i < amigos.length; i++){
+    lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}
+
+// sortear amigos 
+function sortearAmigo(){
+    let index;
+    let amigoSorteado;
+    let resultado;
+    if(amigos.lenght > 0){
+        index = Math.floor(Math.random()* amigos.length +1);
+        amigoSorteado = amigos[index];
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = `<li>O amigo secreto sorteado é: ${amigoSorteado}</li>`;
+    }else{
+        adicionarAmigo();
+    }
 }
