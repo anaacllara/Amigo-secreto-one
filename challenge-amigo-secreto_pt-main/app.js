2afdb1b7039aabc1e20ea7fd434e05fc87cbbdf0
anaacllara <anaclarafc09@gmail.com>
2025-02-06 09:47:24 -0300

    adicionar array e agregar nomes

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,19 @@
+//array para armazenar os nomes 
+let lista=[];
+let nomes;
+function agregar_amigos(){
+    nomes = document.getElementById('amigo');
+    if(lista.includes(nome)){
+        alert('O nome já existe, por favor digitar outro');
+    }else if(nomes.value = ''){
+        alert('Por favor, insira um nome');
+    }else{
+        lista.push(nomes);
+    }
+    limparCampo();
+}
 
+function limparCampo(){
+    campo = document.querySelector('input');
+    campo.value = '';
+}
